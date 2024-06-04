ade4108b70b6a7c5166d3f6085263a29a61bb3ae
Shraban Chakma <shrabanchakma328@gmail.com>
2024-06-04 06:57:59 +0600

    feat: add stripe payment gateaway

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
-const stripe = require("stripe")(process.env.PK_STRIPE);
+const stripe = require("stripe")(process.env.REACT_APP_SECRET_KEY);
 const uuid = require("uuid");
 const app = express();
-
+console.log(process.env.REACT_APP_SECRET_KEY);
 // middleware
 app.use(express.json());
 app.use(cors());
@@ -13,6 +13,39 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("It really works");
 });
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("product", product);
+  console.log("price", product.price);
+  //   create a unique idempontency key in case we don't double charge
+  //   the user due to network issue
+  const idempontencyKey = uuid.v4();
 
+  //   create a customer using stripe.customers.create
+  //   validate the token
+  //   if validation done charge the user
+  // stripe.charges.create({product_details}, {idempontencykey}
+  //   customer related doc:https://docs.stripe.com/api/customers
+  //   charge related doc: https://docs.stripe.com/api/charges/create
+  return stripe.customers
+    .create({
+      email: token.email,
+      id: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product?.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: product.name,
+        },
+        { idempontencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
 // listen
 app.listen(8000, () => console.log("listening at port 8000"));
